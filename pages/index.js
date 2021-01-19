c73d0cc905d04f9af3d511316b018b11f5a7c57f
Phil Mottin <philmotin@gmail.com>
2021-01-19 18:59:52 -0300

    new  home, add counter page

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,7 @@
-import { userState, useState } from 'react';
+
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Contador />
-        </div>
-    )
+    return <h1>Home</h1>
 }
 
-function Contador() {
-    const [contador, setContador] = useState(1);
- 
-    function adicionarContador(){
-        setContador(contador+1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 export default Home
